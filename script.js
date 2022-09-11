0a685d6ea66491c265209a57a7c58570786bde3c
Felipe Bezerra <felipbsousa036@gmail.com>
2022-09-11 16:13:54 -0400

    commit inicial

---
+++ script.js
@@ -0,0 +1,31 @@
+const change = document.querySelector(".change");
+const move = document.querySelector(".move");
+const hidden = document.querySelector(".hidden");
+const container = document.querySelector(".container");
+const gato = document.querySelector(".gato");
+
+function random(){
+  const min = 100;
+  const max = 1000;
+  const random = Math.floor(Math.random() * (max + min) - min);
+  return random;
+}
+
+container.addEventListener('click', (e) => {
+  if(e.target.classList.contains('change')){
+    gato.style.cssText = 
+    'display: block'
+  }
+})
+
+container.addEventListener('mouseover', (e) => {
+  const elemento = e.target;
+  // console.log(elemento)
+  if (elemento.classList.contains('move')){
+    move.style.cssText = 
+    `left: ${random()}px`,
+    `top: ${random()}px`
+  }
+})
+
+random()
